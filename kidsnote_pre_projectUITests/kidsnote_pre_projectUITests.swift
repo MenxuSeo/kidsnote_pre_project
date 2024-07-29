f255ab4324141a03e82e4cf6c90bd30d8222a8e2
Menxu Seo <m0su-menxuseo@users.noreply.github.com>
2024-07-29 23:09:14 +0900

    chore: tuist 제거

--- kidsnote_pre_projectUITests/kidsnote_pre_projectUITests.swift
+++ kidsnote_pre_projectUITests/kidsnote_pre_projectUITests.swift
@@ -8,34 +8,21 @@
 import XCTest
 
 final class kidsnote_pre_projectUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+  func testNavigation() {
+    // Given
+    let app = XCUIApplication()
+    app.launch()
+    
+    // When
+    app.searchFields.firstMatch.tap()
+    app.searchFields.firstMatch.typeText("1")
+    
+    let firstBookCell = app.tables.cells.firstMatch
+    XCTAssertTrue(firstBookCell.waitForExistence(timeout: 5))
+    firstBookCell.tap()
+    
+    // Then
+    XCTAssertTrue(app.staticTexts["Book Details"].exists)
+    XCTAssertTrue(app.staticTexts["건곤쟁패 1권"].exists)
+  }
 }

0d7b0950b3578e504fec53743034102afd91d55e
Menxu Seo <m0su-menxuseo@users.noreply.github.com>
2024-07-29 23:03:22 +0900

    feat: 날짜에 local format 적용

--- kidsnote_pre_project/Sources/Extensions/Date+.swift
+++ kidsnote_pre_project/Sources/Extensions/Date+.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension DateFormatter {
+  static let jsonDateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd"
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    return formatter
+  }()
+  
+  static let localizedDateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .none
+    formatter.locale = Locale.current
+    return formatter
+  }()
+}
